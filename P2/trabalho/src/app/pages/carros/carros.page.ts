9635c02fdc9dee31f3e0027a88ec3966b780db53
matheus1519 <1519academico@gmail.com>
2020-01-29 13:40:52 -0300

    trabalho p2

--- P2/trabalho/src/app/pages/carros/carros.page.ts
+++ P2/trabalho/src/app/pages/carros/carros.page.ts
@@ -1,35 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Carro } from 'src/app/models/carros';
-import { ApiCarrosService } from 'src/app/service/api-carros.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Carro } from "src/app/models/carros";
+import { ApiCarrosService } from "src/app/service/api-carros.service";
+import { Observable } from "rxjs";
 @Component({
- selector: 'app-carros',
- templateUrl: './carros.page.html',
- styleUrls: ['./carros.page.scss'],
+  selector: "app-carros",
+  templateUrl: "./carros.page.html",
+  styleUrls: ["./carros.page.scss"]
 })
 export class CarrosPage implements OnInit {
-
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
-  }
+  ngOnInit() {}
 
   carros: Observable<any>;
   carro: Carro;
 
-  constructor(private router: Router, private api:ApiCarrosService ) {
+  constructor(private router: Router, private api: ApiCarrosService) {
     this.getAll();
   }
   async getAll() {
-    this.carros = this.api.getAll();
+    this.carros = await this.api.getAll();
   }
-  addCarro(){
-    this.router.navigate(['form-carros', 0]);
+  addCarro() {
+    this.router.navigate(["form-carros", 0]);
   }
   editCarro(id: number) {
-    this.router.navigate(['form-carros', id]);
+    this.router.navigate(["form-carros", id]);
   }
-  removeCarro(id: number){
-    this.api.delete(id).subscribe(data => { this.getAll() });
+  removeCarro(id: number) {
+    this.api.delete(id).subscribe(data => {
+      this.getAll();
+    });
   }
 }
--- P2/trabalho/src/app/tabs/tabs-routing.module.ts
+++ P2/trabalho/src/app/tabs/tabs-routing.module.ts
@@ -1,53 +1,57 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TabsPage } from './tabs.page';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TabsPage } from "./tabs.page";
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: "tabs",
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        children: [
-        {
-        path: '',
-        loadChildren: () =>
-        import('../pages/carros/carros.module').then(m => m.CarrosPageModule)
-        }
-        ]
-       },
-      {
-        path: 'form-carros',
+        path: "carros",
         children: [
           {
-            path: '',
+            path: "",
             loadChildren: () =>
-              import('../pages/form-carros/form-carros.module').then(m => m.FormCarrosPageModule)
+              import("../pages/carros/carros.module").then(
+                m => m.CarrosPageModule
+              )
           }
         ]
       },
       {
-        path: 'tab3',
+        path: "form-carros",
         children: [
           {
-            path: '',
+            path: "",
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import("../pages/form-carros/form-carros.module").then(
+                m => m.FormCarrosPageModule
+              )
           }
         ]
       },
       {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
+        path: "tab3",
+        children: [
+          {
+            path: "",
+            loadChildren: () =>
+              import("../tab3/tab3.module").then(m => m.Tab3PageModule)
+          }
+        ]
+      },
+      {
+        path: "",
+        redirectTo: "/tabs/tab1",
+        pathMatch: "full"
       }
     ]
   },
   {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "/tabs/tab1",
+    pathMatch: "full"
   }
 ];
 
