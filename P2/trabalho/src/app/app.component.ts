7fa3ccac603b792e325567fe2346f283350d56c3
matheus1519 <1519academico@gmail.com>
2020-02-04 17:48:53 -0300

    Trabalho Final

--- P2/trabalho/src/app/app.component.ts
+++ P2/trabalho/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
+  constructor(private router:Router,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    this.router.navigate(["/tabs/carros"]);
   }
 
   initializeApp() {
